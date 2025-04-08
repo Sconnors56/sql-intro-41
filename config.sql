118f2cf042b619004cc6fea0c5a025c76d56625b
Sean Connors <sean.connors@kellogg.northwestern.edu>
2025-04-08 15:32:55 +0000

    Baseball SQL Updates

--- config.sql
+++ config.sql
@@ -1,6 +1,6 @@
 -- Configure mode and headers output
 -- Note: only needs to be configured once
-
+-- One file is for one purpose - that's why everything is broken out in the SQL files
 .mode columns
 -- run `.help .mode` to view other options
 
--- demo.sql
+++ demo.sql
@@ -1,4 +1,6 @@
 -- Get the first and last name of every player in the database.
 -- Note: this is a code comment in SQL (meaning it's documentation and ignored by the computer).
+-- My comment: command / will make new comments or toggle it on/off
+-- Much easier to make changes to up in the top section (plus you keep version control, versus the terminal but it executes all the same)
 
 SELECT first_name, last_name FROM players;
